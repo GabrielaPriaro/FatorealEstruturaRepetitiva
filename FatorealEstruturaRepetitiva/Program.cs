2faabfd007abd6823fdd18f8146c62af4c9d4e76
Gabriela <gabriela.priaro@gmail.com>
2024-10-29 20:18:33 +0000

    Adicionar arquivos de projeto.

---
+++ FatorealEstruturaRepetitiva/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FatorealEstruturaRepetitiva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
